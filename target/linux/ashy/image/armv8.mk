d4e2823cb7a0f4565ea68e513036eea09f1767af
Carlyon W. Tung <loongs@syuusui.com>
2023-06-06 19:38:29 +0800

    Ombres

--- target/linux/ashy/image/armv8.mk
+++ target/linux/ashy/image/armv8.mk
@@ -1,43 +1,13 @@
-# SPDX-License-Identifier: GPL-2.0-only
 #
-# Copyright (C) 2020 Tobias Maedel
+# Copyright (C) 2015 - 2029. Hydra Project.
+#
 
-define Device/friendlyarm_nanopi-r2s
-  DEVICE_VENDOR := FriendlyARM
-  DEVICE_MODEL := NanoPi R2S
-  SOC := rk3328
-  UBOOT_DEVICE_NAME := nanopi-r2s-rk3328
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r2s | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-usb-net-rtl8152
+define Device/ombres_monopi-r5s
+  DEVICE_VENDOR := Ombres
+  DEVICE_MODEL := MonoPi R5S
+  SOC := rk3568
+  UBOOT_DEVICE_NAME := monopi-r5s-rk3568
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script monopi-r5s | pine64-img | gzip | append-metadata
+  DEVICE_PACKAGES := kmod-r8125
 endef
-TARGET_DEVICES += friendlyarm_nanopi-r2s
-
-define Device/friendlyarm_nanopi-r4s
-  DEVICE_VENDOR := FriendlyARM
-  DEVICE_MODEL := NanoPi R4S
-  DEVICE_VARIANT := 4GB LPDDR4
-  SOC := rk3399
-  UBOOT_DEVICE_NAME := nanopi-r4s-rk3399
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script nanopi-r4s | pine64-img | gzip | append-metadata
-  DEVICE_PACKAGES := kmod-r8169
-endef
-TARGET_DEVICES += friendlyarm_nanopi-r4s
-
-define Device/pine64_rockpro64
-  DEVICE_VENDOR := Pine64
-  DEVICE_MODEL := RockPro64
-  SOC := rk3399
-  UBOOT_DEVICE_NAME := rockpro64-rk3399
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
-endef
-TARGET_DEVICES += pine64_rockpro64
-
-define Device/radxa_rock-pi-4a
-  DEVICE_VENDOR := Radxa
-  DEVICE_MODEL := ROCK Pi 4A
-  SOC := rk3399
-  SUPPORTED_DEVICES := radxa,rockpi4a radxa,rockpi4
-  UBOOT_DEVICE_NAME := rock-pi-4-rk3399
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script | pine64-img | gzip | append-metadata
-endef
-TARGET_DEVICES += radxa_rock-pi-4a
+TARGET_DEVICES += ombres_monopi-r5s
