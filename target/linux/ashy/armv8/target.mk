cb8b561e2a5520af24dfbbd381e205137339437f
Carlyon W. Tung <loongs@syuusui.com>
2023-06-06 19:45:27 +0800

    Ombres

--- target/linux/ashy/armv8/target.mk
+++ target/linux/ashy/armv8/target.mk
@@ -1,8 +1,8 @@
 ARCH:=aarch64
 SUBTARGET:=armv8
-BOARDNAME:=RK3568 boards (64 bit)
+BOARDNAME:=AC3645 Boards
 
 define Target/Description
-	Build firmware image for Rockchip RK3568 devices.
+	Build firmware image for RockChip RK3568 devices.
 	This firmware features a 64 bit kernel.
 endef
