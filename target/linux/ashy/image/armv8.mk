bedd78ede99640225b4fb0bc42d3f93fa997e6e3
Carlyon W. Tung <loongs@syuusui.com>
2023-06-24 15:42:55 +0800

    Ashy

--- target/linux/ashy/image/armv8.mk
+++ target/linux/ashy/image/armv8.mk
@@ -4,10 +4,10 @@
 
 define Device/ombres_monopi-r5s
   DEVICE_VENDOR := Ombres
-  DEVICE_MODEL := Ombres MonoPi R5S
+  DEVICE_MODEL := MonoPi R5S
   SOC := rk3568
   UBOOT_DEVICE_NAME := monopi-r5s-rk3568
-  IMAGE/sysupgrade.img.gz := boot-common | boot-script monopi-r5s | pine64-img | gzip
+  IMAGE/sysupgrade.img.gz := boot-common | boot-script monopi-r5s | pine64-img | gzip | append-metadata
   DEVICE_PACKAGES := kmod-usb-net-rtl8152
 endef
 TARGET_DEVICES += ombres_monopi-r5s
